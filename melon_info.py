0a81fd8bfc020881c83b2832c6ad6b397100264b
NOURSAR <96560657+NOURSAR@users.noreply.github.com>
2022-02-18 08:08:52 +0000

    Initial commit

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,19 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon_names
 
 
-def print_melon(name, seedless, price):
+def print_melon(name):
     """Print each melon with corresponding attribute information."""
+    
+    for key_melon, value_melon in melon_names.items():  #will get an error without .items()
+        print(key_melon.upper())
+        for key in value_melon:
+            print(f'{key} : {value_melon[key]}')
+print_melon(melon_names)
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+#print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+#n = list(melon_names.keys())
+#m = list(melon_names.values())
+#print(melon_names['Honeydew']['price'])
--- melons.py
+++ melons.py
@@ -1,23 +1,42 @@
 melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
+    'Honeydew': { 'price': 0.99,
+                  'seedlessness': True,
+                  'flesh_color': None,
+                  'wiegt': None,
+                  'rind_color': None,
+                  'source': None
+                },
+    'Crenshaw': {'price': 2.00,
+                 'seedlessness': False,
+                 'flesh_color': None,
+                 'wieght': None,
+                 'rind_color': None,
+                 'source': None
+                },
+    'Crane':    {'price': 2.50,
+                 'seedlessness': False,
+                 'flesh_color': None,
+                 'wieght': None,
+                 'rind_color': None,
+                 'source': None,
+                },
+    'Casaba':   {'price': 2.50,
+                 'seedlessness': False,
+                 'flesh_color': None,
+                 'wieght': None,
+                 'rind_color': None,
+                 'source': None
+                },
+    'Cantaloupe':{'price': 2.00,
+                 'seedlessness': False,
+                 'flesh_color': None,
+                 'wieght': None,
+                 'rind_color': None,
+                 'source': None
+                 }
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+for k, v in melon_names.items():  #will get an error without .items()
+    print(k)
+    for b in v:
+        print(b, v[b])
